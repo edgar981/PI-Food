c555be83a278dbe741907618b2252b0a7fa04bbd
Edgar <davidnavarro81230@outlook.com>
2023-02-08 00:11:23 -0500

    j

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -6,6 +6,7 @@ import "./searchbar.css"
 
 export default function SearchBar() {
     const [recipe, setRecipe] = useState("");
+    const [isActive, setIsActive] = useState(false);
     const dispatch = useDispatch();
 
     const handleInput = (evento) => {
@@ -24,11 +25,14 @@ export default function SearchBar() {
         //     />
         //     <button onClick={(e) => onSearch(e)}>Buscar</button>
         // </div>
-        <div className="searchbar">
-            <div className="icon"></div>
-            {/*<input type='text' name='search' placeholder='type name'*/}
-            {/*       onChange={(e)=> handleInput(e)} value={recipe}*/}
-            {/*/>*/}
+        <div className={`searchbar ${isActive && 'active'}`}>
+            <div className="icon" onClick={()=>setIsActive(!isActive)}></div>
+            <div className='input'>
+                <input type='text' name='search' placeholder='Search'
+                       onChange={(e)=> handleInput(e)} value={recipe}
+                />
+            </div>
+            <span className='go'></span>
             {/*<button onClick={(e) => onSearch(e)}>Buscar</button>*/}
         </div>
     );
